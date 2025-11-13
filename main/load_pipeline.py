682682084b18d5947203e5866eeac344d3021a0b
davidop97 <davidalexanderp31@hotmail.com>
2025-11-13 09:59:49 -0500

    notebooks folder was created

--- main/load_pipeline.py
+++ main/load_pipeline.py
@@ -1,25 +1,23 @@
 from typing import Dict
-import joblib  # type: ignore
 import pandas as pd
 from main.plotter import plot_data
-from turbine_pipeline import TurbineDataPipeline
+from data_preprocessing.preprocessing_pipeline import TurbineDataPipeline
 
 
-def _load_pipeline(pipeline_path: str) -> TurbineDataPipeline:
+def preprocess_data(file_path: str) -> tuple[pd.DataFrame, pd.DataFrame, float, Dict[str, float]]:
     """
-    Carga el pipeline de procesamiento desde un archivo.
+    Genera los dataframes utilizando una nueva instancia del pipeline de procesamiento.
+    
+    Args:
+        file_path (str): Ruta al archivo CSV de datos
+        
+    Returns:
+        tuple: (df_original, df_processed, nominal_speed, max_values)
     """
-    pipeline = joblib.load(pipeline_path)
-    return pipeline
-
-
-def preprocess_data(file_path: str, pipeline_path: str) -> tuple[pd.DataFrame, pd.DataFrame, float, Dict[str, float]]:
-    """
-    Genera los dataframes a partir del pipeline de procesamiento.
-    Los retorna
-    """
-    pipeline = _load_pipeline(pipeline_path)
-    # Procesar el archivo y obtener los DataFrames  
+    # Crear una nueva instancia del pipeline
+    pipeline = TurbineDataPipeline()
+    
+    # Procesar el archivo y obtener los DataFrames
     df_processed, nominal_speed, max_values = pipeline.fit_transform(file_path)
     df_original = pipeline.get_original_data()
     return df_original, df_processed, nominal_speed, max_values
@@ -28,8 +26,7 @@ def preprocess_data(file_path: str, pipeline_path: str) -> tuple[pd.DataFrame, p
 if __name__ == "__main__":
     # Procesar un archivo
     file_path = "Histórico, Tendencia_ UNIDAD 1 (1).csv"
-    pipeline_path = "turbine_pipeline.joblib"
-    df_original, df_processed, nominal_speed, max_values = preprocess_data(file_path, pipeline_path)
+    df_original, df_processed, nominal_speed, max_values = preprocess_data(file_path)
 
     # Graficar los datos procesados
     plot_data(df_processed, parent_window=None)
